75b82a27798bf6f68e4f57dfec9b5d2258d54907
Tagussan <jap.yoshi@gmail.com>
2015-09-11 00:55:57 +0900

    changed name

--- gen4x4Wiring.rb
+++ gen4x4Wiring.rb
@@ -16,20 +16,20 @@ moves = [left, up, right, down]
 moves.map! {|i|
     i.flatten
 }
-puts "4x4 -> left merge map"
+puts "rotate"
 for i in 0 ... moves.length
-    puts "if (move == #{i}) begin" if i == 0
-    puts " else if (move == #{i}) begin" if i != 0
+    puts "if (dir == #{i}) begin" if i == 0
+    puts " else if (dir == #{i}) begin" if i != 0
     for j in 0 ... moves[i].length
         puts "    Y_all[#{j*5+4}:#{j*5}] <= X_all[#{moves[i][j]*5+4}:#{moves[i][j]*5}];"
     end
     print "end"
 end
 puts ""
-puts "left merge map -> 4x4"
+puts "undo rotate"
 for i in 0 ... moves.length
-    puts "if (move == #{i}) begin" if i == 0
-    puts " else if (move == #{i}) begin" if i != 0
+    puts "if (dir == #{i}) begin" if i == 0
+    puts " else if (dir == #{i}) begin" if i != 0
     for j in 0 ... moves[i].length
         puts "    Y_all[#{moves[i][j]*5+4}:#{moves[i][j]*5}] <= X_all[#{j*5+4}:#{j*5}];"
     end
