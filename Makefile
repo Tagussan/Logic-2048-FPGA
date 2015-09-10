d9561a7f2ba73c93c6491d4c88d5b2419cf8c2c9
Tagussan <jap.yoshi@gmail.com>
2015-09-10 18:45:03 +0900

    added Makefile; single line merge works

---
+++ Makefile
@@ -0,0 +1,19 @@
+MAIN = logic2048
+BENCH = tb_logic2048
+SOURCES = *.v
+VERILOG = iverilog
+WAVE = gtkwave
+
+.PHONY: all
+all: $(TOP).vcd
+
+$(TOP).vcd:
+	$(VERILOG) -o $(MAIN).out -s $(BENCH) $(SOURCES)
+	./$(MAIN).out
+
+wave: $(TOP).vcd
+	$(WAVE) $(MAIN).vcd > /dev/null 2>&1 &
+
+.PHONY: clean
+clean:
+	rm -rf *.vcd $(MAIN).out
---
+++ tb_logic2048SingleLine.v
@@ -0,0 +1,46 @@
+`timescale 1ns/1ns
+module tb_logic2048SingleLine();
+    parameter STEP = 20;
+    reg [3:0] x0,x1,x2,x3;
+    wire [3:0] y0,y1,y2,y3;
+    logic2048SingleLine md(x0,x1,x2,x3,y0,y1,y2,y3);
+    initial begin
+        $dumpfile("logic2048.vcd");
+        $dumpvars(0, tb_logic2048);
+        x0 = 0;
+        x1 = 0;
+        x2 = 0;
+        x3 = 0;
+        x0 = 0;
+        x1 = 1;
+        x2 = 2;
+#STEP   x3 = 0;
+        x0 = 1;
+        x1 = 1;
+        x2 = 2;
+#STEP   x3 = 0;
+        x0 = 1;
+        x1 = 1;
+        x2 = 1;
+#STEP   x3 = 1;
+        x0 = 0;
+        x1 = 0;
+        x2 = 0;
+#STEP   x3 = 1;
+        x0 = 0;
+        x1 = 1;
+        x2 = 0;
+#STEP   x3 = 1;
+        x0 = 2;
+        x1 = 1;
+        x2 = 0;
+#STEP   x3 = 1;
+        x0 = 1;
+        x1 = 2;
+        x2 = 3;
+#STEP   x3 = 4;
+#STEP   x3 = 4;
+        $dumpflush;
+        $finish;
+    end
+endmodule
